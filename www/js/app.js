35bbf380a8775687a5d7339af2dbdc5e7b2e0309
eejv21 <eejv_21@hotmail.com>
2017-08-24 10:21:20 -0500

    este es el primer archivo en git

---
+++ www/js/app.js
@@ -0,0 +1,12 @@
+var baset = 10;
+var alturat = 5;
+var areat = (baset * alturat) / 2;
+console.log("area del traiangulo es: " + areat);
+
+var radio = 5;
+var areac = Math.PI * Math.pow(radio, 2);
+console.log("El área del circulo es: " + areac);
+
+var radio = 10;
+var volues = (4 / 3) * Math.PI * Math.pow(radio, 3);
+console.log("El Volumen De La Esfera es: " + volues);
